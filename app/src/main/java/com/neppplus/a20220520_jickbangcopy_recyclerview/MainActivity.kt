0a30e67f63fa26522ccda6bfe858ee1f22af9756
조경진 <kmc901206@gmail.com>
2022-05-20 12:12:51 +0900

    4. 데이터 목록 추가 / 어댑터 객체화 => 리싸이클러 뷰와 연결

--- app/src/main/java/com/neppplus/a20220520_jickbangcopy_recyclerview/MainActivity.kt
+++ app/src/main/java/com/neppplus/a20220520_jickbangcopy_recyclerview/MainActivity.kt
@@ -2,10 +2,41 @@ package com.neppplus.a20220520_jickbangcopy_recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.a20220520_jickbangcopy_recyclerview.adapters.RoomRecyclerViewAdapter
+import com.neppplus.a20220520_jickbangcopy_recyclerview.models.RoomData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var mRoomList = ArrayList<RoomData>()
+
+    lateinit var mRoomAdapter : RoomRecyclerViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+        mRoomList.add( RoomData(8000, "마포구 서교동", 1, "망원/홍대역 풀옵션 넓은 원룸") )
+        mRoomList.add( RoomData(28000, "마포구 서교동", 5, "홍대입구역 풀옵션 투룸") )
+        mRoomList.add( RoomData(12000, "마포구 서교동", 2, "망원/홍대역 인근 신축 원룸 전세") )
+        mRoomList.add( RoomData(12000, "마포구 망원동", 3, "홍대역 풀옵션 넓은 원룸") )
+        mRoomList.add( RoomData(15000, "마포구 망원동", 5, "테라스가 넓은 풀옵션 넓은 원룸") )
+        mRoomList.add( RoomData(13000, "마포구 망원동", 3, "넓고 반듯한 신축 원룸") )
+        mRoomList.add( RoomData(9000, "마포구 연남동", 2, "홍대역 풀옵션 넓은 원룸") )
+        mRoomList.add( RoomData(7500, "마포구 연남동", -2, "연남동 연습 가능 연습실") )
+        mRoomList.add( RoomData(26000, "마포구 연남동", 3, "강추!! 홍대역 테라스 넓은 원룸") )
+        mRoomList.add( RoomData(5500, "마포구 연남동", 0, "홍대역 풀옵션 원룸") )
+
+        mRoomAdapter = RoomRecyclerViewAdapter(this, mRoomList)
+        mainRecyclerView.adapter = mRoomAdapter
+    }
+
 }
